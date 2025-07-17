4b04107de2c8d3f1174668034e5bee77490cab08
Fabrice Van Speybrock <fabrice.vanspeybrock@gmail.com>
2025-07-17 09:17:14 +0200

    MAJ script load_pipeline

--- app/models/load_pipeline.py
+++ app/models/load_pipeline.py
@@ -1,9 +1,7 @@
+from pathlib import Path
 import joblib
-from app.utils.binary_mapper import BinaryMapper
 
 def load_pipeline():
-    """
-    Charge le pipeline ML sauvegardé.
-    """
-    pipeline = joblib.load("C:/Users/Fab/Documents/P5_Déployez_un_modèle_de_Machine_Learning/app/models/pipeline.joblib")
+    model_path = Path(__file__).parent / "pipeline.joblib"
+    pipeline = joblib.load(model_path)
     return pipeline
